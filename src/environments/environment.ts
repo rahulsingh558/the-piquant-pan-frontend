6ef4e0df63852273eee53ed6d2763fac0574ccf4
Rahul Singh <rahulmeyok558@gmail.com>
2026-01-30 17:30:57 +0530

    Update restaurant coordinates to exact location (12.997558, 77.658151)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,8 +8,8 @@ export const environment = {
         apiKey: '771985f5b0af98e110a1b13a73e27767',
         restaurantELoc: 'ta34s7',
         restaurantCoords: {
-            lat: 12.9906677,
-            lng: 77.6525905
+            lat: 12.997558,
+            lng: 77.658151
         },
         restaurantAddress: 'Krishnaiyyapalya Main Road, Sadananda Nagar, Benaiganahalli, Bengaluru, Karnataka 560038'
     }
